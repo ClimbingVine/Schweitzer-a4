908af9480e0acab04518e5408510fe8c23b6fb9d
Liana Schweitzer <liana.schweitzer@pcc.edu>
2017-10-19 14:39:59 -0700

    Added main method which prints two sentences

--- src/Schweitzer_a4/a4.java
+++ src/Schweitzer_a4/a4.java
@@ -1,4 +1,11 @@
 package Schweitzer_a4;
-
+/**
+ *  Created by Liana Schweitzer on 10/19/2017
+ */
 public class a4 {
+    public static void main(String args[]) {
+        System.out.print("I need to add some print statements for this section");
+        System.out.println(" of the assignment.");
+        System.out.println("Assignment #4 that is.");
+    }
 }
